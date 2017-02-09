d8e2a96c7bcf905df26a655eec91c007e9f8f6ec
Wendell Indestructible <chen-gz@outlook.com>
2017-02-09 17:32:56 +0800

    HH

--- template/素数.cpp
+++ template/素数.cpp
@@ -1,16 +1,16 @@
-//#include<cstring>
-//#define maxn 555555
-//int mark[maxn], prime[maxn], res;
-//void get_prime(int n) {
-//	memset(mark, 0, sizeof(mark));
-//	res = 0;
-//	for (int i = 2; i <= n; i++) {
-//		if (!mark[i])
-//			mark[i] = prime[res++] = i;
-//		for (int j = 0; j < res && prime[j] * i <= n; j++) {
-//			mark[i*prime[j]] = prime[j];
-//			if (i%prime[j] == 0)
-//				break;
-//		}
-//	}
-//}
+#include <cstring>
+#define maxn 555555
+int mark[maxn], prime[maxn], res;
+void get_prime(int n) {
+	memset(mark, 0, sizeof(mark));
+	res = 0;
+	for (int i = 2; i <= n; i++) {
+		if (!mark[i])
+			mark[i] = prime[res++] = i;
+		for (int j = 0; j < res && prime[j] * i <= n; j++) {
+			mark[i*prime[j]] = prime[j];
+			if (i%prime[j] == 0)
+				break;
+		}
+	}
+}
