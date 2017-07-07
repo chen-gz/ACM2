df335ed27103c3ebb3cd46d3f645c84c86b7b9c2
Wendell <i.cc.wendell@gmail.com>
2017-07-07 10:11:43 +0800

    add cf context 818

---
+++ Codeforces/contest/818/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[]) {
+	FILE *fr = freopen("/home/wendell/Program/in", "r", stdin),
+	      *fo = freopen("/home/wendell/Program/out", "w", stdout);
+	std::ios::sync_with_stdio(false);
+	long long n, k;
+	cin >> n >> k;
+	int l = 0 , r = n;
+	while (r - l > 0) {
+
+	}
+	cout << 0 << ' ' << 0 << ' ' << n << endl;
+	return 0;
+}
